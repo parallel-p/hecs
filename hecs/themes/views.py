c7181a56c4399cf49db22af98bd8d8d772226d19
Ivan Kriuchkov <johnhook059@gmail.com>
2016-01-05 11:53:21 +0300

    now its work

--- hecs/themes/views.py
+++ hecs/themes/views.py
@@ -4,13 +4,14 @@ from .models import Theme, Reference
 
 def theme_page(request, theme_id):
     theme = get_object_or_404(Theme, pk=theme_id)
-    references = list(Reference.objects.filter(theme=theme))
+    references = list(Reference.objects.filter(theme=theme).all())
     groups = {}
     for reference in references:
-        if reference.group not in groups.keys():
+        if reference.group.name not in groups.keys():
             groups[reference.group.name] = []
         groups[reference.group.name].append(reference)
 
+
     return render(request, 'theme.html', {'name': theme.name,
-                                          'groups': sorted(groups.items(), key=lambda x: (x[0], x[1].target.name))})
+                                          'groups': sorted(groups.items(), key=lambda x: x[0])})
 
