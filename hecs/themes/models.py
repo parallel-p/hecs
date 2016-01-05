7b5e5cfeecbb880b1b54c321ab58e6174dcbf417
Ivan Kriuchkov <johnhook059@gmail.com>
2016-01-05 09:55:11 +0300

    #2 and .gitignore

--- hecs/themes/models.py
+++ hecs/themes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Theme(models.Model):
+    name = models.CharField(max_length=30, blank=True, default='')
+    x = models.IntegerField(null=True)
+    y = models.IntegerField(null=True)
+    color = models.CharField(max_length=6, blank=True, default='')
+
+    def __str__(self):
+        return self.name
