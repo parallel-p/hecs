6cc30292ec33ff62ce09d6d2e49a856f8811efc9
Denis Kalinochkin <kalinochkind@gmail.com>
2016-01-05 19:45:33 +0300

    Fixes

--- hecs/themes/admin.py
+++ hecs/themes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Theme)
+admin.site.register(ReferenceGroup)
+admin.site.register(ReferenceTarget)
+admin.site.register(Reference)
+admin.site.register(Blank)
+admin.site.register(Comment)
