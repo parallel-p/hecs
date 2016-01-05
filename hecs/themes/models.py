96ef46754b67caed72dca212dcd8aaee27d27011
SpyCheese <mikle98@yandex.ru>
2016-01-05 11:41:09 +0300

    #3

--- hecs/themes/models.py
+++ hecs/themes/models.py
@@ -5,7 +5,8 @@ class Theme(models.Model):
     name = models.CharField(max_length=30, blank=True, default='')
     x = models.IntegerField(null=True)
     y = models.IntegerField(null=True)
-    color = models.CharField(max_length=6, blank=True, default='')
+    color = models.CharField(max_length=20, blank=True, default='')
 
     def __str__(self):
         return self.name
+
--- hecs/themes/views.py
+++ hecs/themes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Theme
 
-# Create your views here.
+HOMEPAGE_ROWS = 10
+HOMEPAGE_COLS = 16
+
+def home_page(request):
+    themes = Theme.objects.all()
+    rows = [[row, [[col, None] for col in range(HOMEPAGE_COLS)]] for row in range(HOMEPAGE_ROWS)]
+    for theme in themes:
+        try:
+            print (theme.x, theme.y)
+            rows[theme.x][1][theme.y][1] = theme
+        except IndexError:
+            pass
+    return render(request, 'homepage.html', {'rows': rows})
