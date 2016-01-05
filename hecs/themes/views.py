6b6508677eec43234fb8d2726d779f766fad35e4
Denis Kalinochkin <kalinochkind@gmail.com>
2016-01-05 12:54:05 +0300

    #12

--- hecs/themes/views.py
+++ hecs/themes/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Theme, Reference
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from .forms import SignupForm, LoginForm
+from django.db import IntegrityError
 
 HOMEPAGE_ROWS = 10
 HOMEPAGE_COLS = 16
@@ -28,3 +32,35 @@ def theme_page(request, theme_id):
     return render(request, 'theme.html', {'name': theme.name,
                                           'groups': sorted(groups.items(), key=lambda x: x[0])})
 
+
+def signup_page(request):
+    if request.user.is_authenticated():
+        return redirect('/home')
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        try:
+            user = User.objects.create_user(form.data['login'], password=form.data['password'], first_name=form.data['firstname'], last_name=form.data['lastname'])
+            user.save()
+        except IntegrityError:
+            return redirect('/signup?login_used')
+        user = authenticate(username=form.data['login'], password=form.data['password'])
+        login(request, user)
+        return redirect('/home')
+    else:
+        return render(request, 'signup_form.html', {'login_used': 'login_used' in request.GET})
+    
+def login_page(request):
+    if request.user.is_authenticated():
+        return redirect('/home')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        print(form.data)
+        user = authenticate(username=form.data['login'], password=form.data['password'])
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect('/home')
+        else:
+            return redirect('/login?login_error')
+    else:
+        return render(request, 'login_form.html', {'login_error': 'login_error' in request.GET})
